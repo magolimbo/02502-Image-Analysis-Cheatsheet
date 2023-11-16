5a9c22a0bda6fc095e948ebc55a3ad98c06b281b
Gabriellanaro <s233541@dtu.dk>
2023-11-16 17:36:29 +0100

    Solutions and worksheet7

---
+++ Exercise Solutions/Ex7-VideoImageTransformations_solution.py
@@ -0,0 +1,86 @@
+import time
+import cv2
+from skimage.transform import swirl
+from skimage.transform import rotate
+import math
+
+
+def show_in_moved_window(win_name, img, x, y):
+    """
+    Show an image in a window, where the position of the window can be given
+    """
+    cv2.namedWindow(win_name)
+    cv2.moveWindow(win_name, x, y)
+    cv2.imshow(win_name, img)
+
+
+def process_rgb_image(img, counter):
+    """
+    Simple processing of a color (RGB) image
+    """
+    str = math.sin(counter / 10) * 10
+    rad = 300
+    swirl_img = swirl(img, strength=str, radius=rad)
+    return swirl_img
+
+
+def capture_from_camera_and_show_images():
+    print("Starting image capture")
+
+    print("Opening connection to camera")
+    url = 0
+    use_droid_cam = False
+    if use_droid_cam:
+        url = "http://192.168.1.120:4747/video"
+    cap = cv2.VideoCapture(url)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+
+    print("Starting camera loop")
+    # To keep track of frames per second using a high-performance counter
+    old_time = time.perf_counter()
+    fps = 0
+    stop = False
+    counter = 0
+    while not stop:
+        ret, new_frame = cap.read()
+        if not ret:
+            print("Can't receive frame. Exiting ...")
+            break
+
+        # Change from OpenCV BGR to scikit image RGB
+        new_image = new_frame[:, :, ::-1]
+        proc_time_start = time.perf_counter()
+        proc_img = process_rgb_image(new_image, counter)
+        proc_time = time.perf_counter() - proc_time_start
+        # convert back to OpenCV BGR to show it
+        proc_img = proc_img[:, :, ::-1]
+
+        counter = counter + 1
+
+        # update FPS - but do it slowly to avoid fast changing number
+        new_time = time.perf_counter()
+        time_dif = new_time - old_time
+        old_time = new_time
+        fps = fps * 0.95 + 0.05 * 1 / time_dif
+
+        # Put the FPS on the new_frame
+        str_out = f"fps: {int(fps)} proc.time: {int(proc_time * 1000)} ms"
+        font = cv2.FONT_HERSHEY_COMPLEX
+        cv2.putText(new_frame, str_out, (100, 100), font, 1, 255, 1)
+
+        # Display the resulting frame
+        show_in_moved_window('Input', new_frame, 0, 10)
+        show_in_moved_window('Processed image', proc_img, 1200, 10)
+
+        if cv2.waitKey(1) == ord('q'):
+            stop = True
+
+    print("Stopping image loop")
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    capture_from_camera_and_show_images()
---
+++ Exercise Solutions/Ex8_auxiliaryFuns.py
@@ -0,0 +1,113 @@
+import glob
+import os
+import pathlib
+from matplotlib import pyplot as plt
+import numpy as np
+from skimage import io
+from skimage.util import img_as_ubyte
+from skimage.transform import SimilarityTransform
+from skimage.transform import warp
+
+
+def read_landmark_file(file_name):
+    f = open(file_name, 'r')
+    lm_s = f.readline().strip().split(' ')
+    n_lms = int(lm_s[0])
+    if n_lms < 3:
+        print(f"Not enough landmarks found")
+        return None
+
+    new_lms = 3
+    # 3 landmarks each with (x,y)
+    lm = np.zeros((new_lms, 2))
+    for i in range(new_lms):
+        lm[i, 0] = lm_s[1 + i * 2]
+        lm[i, 1] = lm_s[2 + i * 2]
+    return lm
+
+def align_and_crop_one_cat_to_destination_cat(img_src, lm_src, img_dst, lm_dst):
+    """
+    Landmark based alignment of one cat image to a destination
+    :param img_src: Image of source cat
+    :param lm_src: Landmarks for source cat
+    :param lm_dst: Landmarks for destination cat
+    :return: Warped and cropped source image. None if something did not work
+    """
+    tform = SimilarityTransform()
+    tform.estimate(lm_src, lm_dst)
+    warped = warp(img_src, tform.inverse, output_shape=img_dst.shape)
+
+    # Center of crop region
+    cy = 185
+    cx = 210
+    # half the size of the crop box
+    sz = 180
+    warp_crop = warped[cy - sz:cy + sz, cx - sz:cx + sz]
+    shape = warp_crop.shape
+    if shape[0] == sz * 2 and shape[1] == sz * 2:
+        return img_as_ubyte(warp_crop)
+    else:
+        print(f"Could not crop image. It has shape {shape}. Probably to close to border of image")
+        return None
+    
+def preprocess_all_cats(in_dir, out_dir):
+    """
+    Create aligned and cropped version of image
+    :param in_dir: Where are the original photos and landmark files
+    :param out_dir: Where should the preprocessed files be placed
+    """
+    dst = "data/ModelCat"
+    dst_lm = read_landmark_file(f"{dst}.jpg.cat")
+    dst_img = io.imread(f"{dst}.jpg")
+
+    all_images = glob.glob(in_dir + "*.jpg")
+    for img_idx in all_images:
+        name_no_ext = os.path.splitext(img_idx)[0]
+        base_name = os.path.basename(name_no_ext)
+        out_name = f"{out_dir}/{base_name}_preprocessed.jpg"
+
+        src_lm = read_landmark_file(f"{name_no_ext}.jpg.cat")
+        src_img = io.imread(f"{name_no_ext}.jpg")
+
+        proc_img = align_and_crop_one_cat_to_destination_cat(src_img, src_lm, dst_img, dst_lm)
+        if proc_img is not None:
+            io.imsave(out_name, proc_img)
+            
+def preprocess_one_cat():
+    src = "data/MissingCat"
+    dst = "data/ModelCat"
+    out = "data/MissingCatProcessed.jpg"
+
+    src_lm = read_landmark_file(f"{src}.jpg.cat")
+    dst_lm = read_landmark_file(f"{dst}.jpg.cat")
+
+    src_img = io.imread(f"{src}.jpg")
+    dst_img = io.imread(f"{dst}.jpg")
+
+    src_proc = align_and_crop_one_cat_to_destination_cat(src_img, src_lm, dst_img, dst_lm)
+    if src_proc is None:
+        return
+
+    io.imsave(out, src_proc)
+
+    fig, ax = plt.subplots(ncols=3, figsize=(16, 6))
+    ax[0].imshow(src_img)
+    ax[0].plot(src_lm[:, 0], src_lm[:, 1], '.r', markersize=12)
+    ax[1].imshow(dst_img)
+    ax[1].plot(dst_lm[:, 0], dst_lm[:, 1], '.r', markersize=12)
+    ax[2].imshow(src_proc)
+    for a in ax:
+        a.axis('off')
+    plt.tight_layout()
+    plt.show()
+    
+def create_u_byte_image_from_vector(im_vec, height, width, channels):
+    min_val = im_vec.min()
+    max_val = im_vec.max()
+
+    # Transform to [0, 1]
+    im_vec = np.subtract(im_vec, min_val)
+    im_vec = np.divide(im_vec, max_val - min_val)
+    im_vec = im_vec.reshape(height, width, channels)
+    im_out = img_as_ubyte(im_vec)
+    return im_out
