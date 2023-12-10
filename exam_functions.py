706b3b32c2686813dc8ea030731b4bde34651557
Gabriellanaro <s233541@dtu.dk>
2023-12-10 13:29:21 +0100

    Update exam_functions.py

--- exam_functions.py
+++ exam_functions.py
@@ -621,3 +621,33 @@ def lda(class_0_data, class_1_data, new_vector):
 # print(f"Probability for class 1 (Mushroom Type B): {probabilities[1]}")
 
 
+
+# input is a tuple of RGB colour values
+# example: RGB2HSI((100, 150, 200))
+
+def rgb2hsi( colour ): 
+    
+    from math import acos, pi, sqrt
+    
+    (R, G, B) = colour
+       
+    r = R / ((R + 0.000001) + (G + 0.000001) + (B + 0.000001)) 
+    g = G / ((R + 0.000001) + (G + 0.000001) + (B + 0.000001))
+    b = B / ((R + 0.000001) + (G + 0.000001) + (B + 0.000001))
+   
+    num = 0.5 * ((r - g) + (r - b))
+    den = sqrt((r - g)**2 + (r - b) * (g - b))
+    h = acos(num / (den + 0.0000001))
+    
+    if b <= g:
+        h = h
+    else:
+        h = 2 * pi - h
+    
+    H = round(h * 180/pi)
+    
+    S = round(100 * (1 - 3 * min(r, g, b) + 0.000001), 1)
+    
+    I = round((R + G + B)/3)
+         
+    return (H, S, I)
